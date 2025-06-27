468469fe9f5d9130218ad9dc4878fab05a249746
light11014 <83333513+light11014@users.noreply.github.com>
2025-06-27 21:10:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num) {
+        long number = num;
+        for(int i = 0; i < 500; i++) {
+            if(number == 1) return i;
+            else if(number % 2 == 0) number /= 2;
+            else number = number * 3 + 1;
+        }    
+        return -1;
+    }
+}
