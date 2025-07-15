c62894698bc9630a4bf0f164ea543783af926ae0
light11014 <83333513+light11014@users.noreply.github.com>
2025-07-15 17:17:42 +0900

    [Bronze V] Title: A+B - 5, Time: 136 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line;
+        while((line = br.readLine()) != null && !line.isEmpty()) {
+            StringTokenizer st = new StringTokenizer(line);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            if(A == 0 && B == 0) return;
+            System.out.println(A + B);
+        }
+    }
+}
