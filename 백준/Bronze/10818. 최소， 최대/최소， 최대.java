7913a8aa80da846b2fa41ee5a8d0084476171daf
light11014 <83333513+light11014@users.noreply.github.com>
2025-07-21 17:39:32 +0900

    [Bronze III] Title: 최소, 최대, Time: 1192 ms, Memory: 102136 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        System.out.println(nums[0] + " " + nums[N-1]);
+    }
+}
