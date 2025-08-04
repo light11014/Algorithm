f72b423d2b934777a5677cc58de61b9e84269a0e
light11014 <83333513+light11014@users.noreply.github.com>
2025-08-04 14:03:55 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -4,28 +4,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        int ten = 0;
+        
         int two = 0;
         int five = 0;
 
         for(int i = 2; i <= N; i++) {
             int num = i;
-
-            while(num % 10 == 0) {
-                num /= 10;
-                ten++;
-            }
-            while(num % 2 == 0) {
-                num /= 2;
-                two++;
-            }
-            while(num % 5 == 0) {
-                num /= 5;
-                five++;
-            }
+            two += countFactor(num, 2);
+            five += countFactor(num, 5);
         }
 
-        System.out.println(ten + Math.min(two, five));
+        System.out.println(Math.min(two, five));
+    }
+    
+    private static int countFactor(int num, int factor) {
+        int count = 0;
+        while(num % factor == 0) {
+            num /= factor;
+            count++;
+        }
+        return count;
     }
 }
