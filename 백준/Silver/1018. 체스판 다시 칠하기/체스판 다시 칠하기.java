35d1f10ff206b39cdec217d494f7fd90cc3b7613
light11014 <83333513+light11014@users.noreply.github.com>
2025-08-05 12:14:54 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -7,36 +7,36 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        String[][] board = new String[N][M];
+        char[][] board = new char[N][M];
 
         for(int i = 0; i < N; i++) {
-            board[i] = br.readLine().split("");
+            board[i] = br.readLine().toCharArray();
         }
 
         int answer = N * M;
         for(int i = 0; i < N - 7; i++) {
             for(int j = 0; j < M - 7; j++) {
-                answer = Math.min(answer, countRepaint(board, i, j, "W", answer));
-                answer = Math.min(answer, countRepaint(board, i, j, "B", answer));
+                answer = Math.min(answer, countRepaint(board, i, j, 'W'));
+                answer = Math.min(answer, countRepaint(board, i, j, 'B'));
             }
         }
         System.out.println(answer);
     }
 
-    private static int countRepaint(String[][] arr, int startX, int startY, String type, int min) {
+    private static int countRepaint(char[][] arr, int startX, int startY, char first) {
         int count = 0;
-        String even = type;
-        String odd = type.equals("W")? "B" : "W";
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 8; j++) {
-                if(!arr[startX+i][startY+j].equals(((i+j)%2 == 0)? even : odd)) {
+                char expected = (i + j) % 2 == 0 ? first : opposite(first);
+                if(arr[startX+i][startY+j] != expected) {
                     count++;
-                    if(count == min) {
-                        return count;
-                    }
                 }
             }
         }
         return count;
+    }
+
+    private static char opposite(char c) {
+        return (c == 'W') ? 'B' : 'W';
     }
 }
