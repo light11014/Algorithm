5108c51604b98c73c4438cb57ca68dbcf2667c55
light11014 <83333513+light11014@users.noreply.github.com>
2025-10-28 14:50:07 +0900

    [level 2] Title: 미로 탈출, Time: 9.28 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,72 +1,73 @@
 import java.util.*;
 
 class Solution {
-    private static final int[] rx = {0, 0, 1, -1};
-    private static final int[] ry = {1, -1, 0, 0};
-    private static char[][] map;
-    private static int N, M;
+    private static int[] rx = {0, 0, 1, -1};
+    private static int[] ry = {1, -1, 0, 0};
     
-    private class Node{
-        int x;
-        int y;
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
+    private static class Node {
+        int r;
+        int c;
+        
+        Node(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
     }
     
     public int solution(String[] maps) {
-        N = maps.length;
-        M = maps[0].length();
-        map = new char[N][M];
         
-        Node start = null, lever = null;
+        // 시작(S) -> 레버(L)
+        int r1 = bfs('S', 'L', maps);
         
-        for(int i = 0; i < N; i++) {
-            map[i] = maps[i].toCharArray();
-            if(maps[i].contains("S")) {
-                start = new Node(i, maps[i].indexOf("S"));
-            }
-            if(maps[i].contains("L")) {
-                lever = new Node(i, maps[i].indexOf("L"));
-            }
-        }
+        if(r1 == -1) 
+            return -1;
         
-        int stol = bfs(start, 'L');
-        int ltoe = bfs(lever, 'E');
-       
-        return stol * ltoe == 0? -1 : stol + ltoe;
+        // 레버(L) -> 출구(E)
+        int r2 = bfs('L', 'E', maps);
+        return (r2 == -1)? -1 : r1 + r2;
     }
     
-    private int bfs(Node s, char e) {
-        int answer = 0;
+    private int bfs(char start, char end, String[] maps) {
         ArrayDeque<Node> queue = new ArrayDeque<>();
-        int[][] dist = new int[map.length][map[0].length];
         
-        queue.addLast(s);
+        int N = maps.length;
+        int M = maps[0].length();
+        
+        int[][] dist = new int[N][M];
+        
+        // 시작 위치 찾기
+        for(int i = 0; i < N; i++) {
+            int j = maps[i].indexOf(start);
+            if(j != -1) {
+                queue.addLast(new Node(i, j));
+                dist[i][j] = 1;
+            }
+        }
         
         while(!queue.isEmpty()) {
             Node now = queue.pollFirst();
             
-            for(int i=0; i<4; i++) {
-                int nx = now.x + rx[i];
-                int ny = now.y + ry[i];
+            for(int i = 0; i < rx.length; i++) {
+                int nr = now.r + rx[i];
+                int nc = now.c + ry[i];
                 
-                
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M || dist[nx][ny] != 0 || map[nx][ny] == 'X') {
+                if(nr < 0 || nc < 0 || nr >= N || nc >= M)
                     continue;
+                
+                if(maps[nr].charAt(nc) == 'X')
+                    continue;
+                
+                if(dist[nr][nc] == 0) {
+                    queue.addLast(new Node(nr, nc));
+                    dist[nr][nc] = dist[now.r][now.c] + 1;
                 }
                 
-
-                if(map[nx][ny] == e) {
-                    return dist[now.x][now.y] + 1;
-                } else {
-                    queue.addLast(new Node(nx, ny));
-                    dist[nx][ny] = dist[now.x][now.y] + 1;
+                if(maps[nr].charAt(nc) == end) {
+                    return dist[nr][nc] - 1;
                 }
             }
-            
         }
-        return 0;
+        
+        return -1;     
     }
 }
