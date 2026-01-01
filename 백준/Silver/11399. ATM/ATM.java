a8252e024e1cdfb25ef9ab04e00c42695eb23eff
light11014 <83333513+light11014@users.noreply.github.com>
2026-01-02 08:53:16 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+        for(int i = 0; i < N; i++) {
+            answer += arr[i] * (N - i);
+        }
+        System.out.println(answer);
+    }
+}
