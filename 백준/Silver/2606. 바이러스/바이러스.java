6d07743ecd6d6b478246f68b23f78889fea5a67a
light11014 <83333513+light11014@users.noreply.github.com>
2026-01-09 08:46:46 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int L = Integer.parseInt(br.readLine());
+
+        // 연결 그래프
+        ArrayList<ArrayList<Integer>> graph
+                = new ArrayList<>();
+
+        for(int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < L ;i ++) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            // 양방향
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        // dfs, stack, 재귀함수
+        boolean[] visited = new boolean[N+1];
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(1);
+        visited[1] = true;
+        int answer = 0;
+
+        while(!stack.isEmpty()) {
+            // 현재 방문
+            int cur = stack.pop();
+            answer++;
+
+            for(int next : graph.get(cur)) {
+                if(!visited[next]) {
+                    stack.push(next);
+                    visited[next] = true;
+                }
+            }
+        }
+
+        System.out.println(answer-1);
+    }
+}
