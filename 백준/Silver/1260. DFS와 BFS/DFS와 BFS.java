631b520ef02634ef27e25bababd7bcecce15c31b
light11014 <83333513+light11014@users.noreply.github.com>
2026-01-25 16:35:58 +0900

    [Silver II] Title: DFS와 BFS, Time: 200 ms, Memory: 19460 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,71 +1,73 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    private static int N;
-    private static int[][] map;
-    private static boolean[] vistied;
-
-    private static StringBuilder sb;
+    static int N;
+    static List<Integer>[] graph;
+    static boolean[] vistied;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-
-        sb = new StringBuilder();
-
-        map = new int[N+1][N+1];
-        vistied = new boolean[N+1];
-
         int M = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
 
-        while (M-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            map[i][j] = 1;
-            map[j][i] = 1;
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        vistied[V] = true;
-        sb.append(V);
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        vistied = new boolean[N+1];
         dfs(V);
+
         sb.append("\n");
+
+        vistied = new boolean[N+1];
         bfs(V);
+
         System.out.println(sb);
     }
 
     private static void dfs(int v) {
-        for (int i = 1; i < map.length; i++) {
-            if(map[v][i] == 1 && !vistied[i]) {
-                vistied[i] = true;
-                sb.append(" ").append(i);
-                dfs(i);
+        vistied[v] = true;
+        sb.append(v).append(" ");
+
+        for (int next : graph[v]) {
+            if (!vistied[next]) {
+                dfs(next);
             }
         }
     }
 
     private static void bfs(int v) {
-        vistied = new boolean[N+1];
-
         ArrayDeque<Integer> queue = new ArrayDeque<>();
-
         queue.add(v);
         vistied[v] = true;
-        sb.append(v);
 
         while (!queue.isEmpty()) {
             int cur = queue.poll();
+            sb.append(cur).append(" ");
 
-            for (int i = 1; i < map.length; i++) {
-                if(map[cur][i] == 1 && !vistied[i]) {
-                    vistied[i] = true;
-                    queue.add(i);
-                    sb.append(" ").append(i);
+            for (int next : graph[cur]) {
+                if (!vistied[next]) {
+                    vistied[next] = true;
+                    queue.add(next);
                 }
             }
         }
