769b26c4406a175adf8f1ad03d6582dc78b67b71
light11014 <83333513+light11014@users.noreply.github.com>
2026-03-10 08:35:26 +0900

    [Silver III] Title: N번째 큰 수, Time: 656 ms, Memory: 219248 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -10,14 +10,16 @@ public class Main {
 
         int[][] table = new int[N][N];
 
+        StringTokenizer st;
+
         for(int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++) {
-                table[i][j] = Integer.parseInt(input[j]);
+                table[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> b.value - a.value);
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(table[b.row][b.col], table[a.row][a.col]));
 
         for(int i = 0; i < N; i++) {
             pq.add(new Node(N-1, i, table[N-1][i]));
@@ -28,14 +30,13 @@ public class Main {
             if(pq.isEmpty()) break;
 
             Node cur = pq.poll();
-            answer = cur.value;
+            answer = table[cur.row][cur.col];
 
             int nr = cur.row-1;
             int nc = cur.col;
 
-            if(nr < 0 || nc < 0) continue;
-
-            pq.add(new Node(nr, nc, table[nr][nc]));
+            if(nr >= 0)
+                pq.add(new Node(nr, nc, table[nr][nc]));
         }
 
         System.out.println(answer);
@@ -44,12 +45,10 @@ public class Main {
     static class Node {
         int row;
         int col;
-        int value;
 
         public Node(int row, int col, int value) {
             this.row = row;
             this.col = col;
-            this.value = value;
         }
     }
 
