a740602250bc516162222588f5b51e230cb87a6c
light11014 <83333513+light11014@users.noreply.github.com>
2026-04-06 13:59:52 +0900

    [Gold IV] Title: 연구소, Time: 392 ms, Memory: 83364 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -7,12 +7,14 @@ public class Main {
     static int M;
     static int answer = 0;
 
+    static List<Point> emptyList = new ArrayList<>();
+    static List<Point> virusList = new ArrayList<>();
+
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
@@ -25,86 +27,72 @@ public class Main {
 
             for(int j = 0; j < M; j++) {
                 lab[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
                 if(lab[i][j] == 0) {
-                    lab[i][j] = 1;
-                    dfs(i, j, 1);
-                    lab[i][j] = 0;
+                    emptyList.add(new Point(i, j));
+                } else if(lab[i][j] == 2) {
+                    virusList.add(new Point(i, j));
                 }
             }
         }
+
+        dfs(0, 0);
 
         System.out.println(answer);
 
     }
 
-    static class Virus {
+    static class Point {
         int x, y;
 
-        public Virus(int x, int y) {
+        public Point(int x, int y) {
             this.x=x;
             this.y=y;
         }
     }
 
-    private static void dfs(int x, int y, int depth) {
+    private static void dfs(int start, int depth) {
         if(depth == 3) {
-            // 바이러스 확인
             answer = Math.max(answer, bfs());
             return;
         }
 
-        for(int i = x; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                if(i == x && j <= y) continue;
+        for(int i = start; i < emptyList.size(); i++) {
+            Point p = emptyList.get(i);
 
-                if(lab[i][j] == 0) {
-                    lab[i][j] = 1;
-                    dfs(i, j, depth + 1);
-                    lab[i][j] = 0;
-                }
-            }
+            lab[p.x][p.y] = 1;
+            dfs(i + 1, depth + 1);
+            lab[p.x][p.y] = 0;
         }
     }
 
     private static int bfs() {
         boolean[][] visited = new boolean[N][M];
-        Queue<Virus> queue = new ArrayDeque<>();
+        Queue<Point> queue = new ArrayDeque<>();
 
-        int wallCount = 0;
-
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                if(lab[i][j] == 2) {
-                    queue.add(new Virus(i, j));
-                } else if (lab[i][j] == 1) {
-                    wallCount++;
-                }
-            }
+        for(Point virus : virusList) {
+            queue.add(virus);
+            visited[virus.x][virus.y] = true;
         }
 
-        int virusCount = queue.size();
+        int infectedCount = 0;
 
         while(!queue.isEmpty()) {
-            Virus cur = queue.poll();
+            Point cur = queue.poll();
 
             for(int i = 0; i < 4; i++) {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
 
                 if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-                if(lab[nx][ny] != 0 || visited[nx][ny]) continue;
+                if(visited[nx][ny] || lab[nx][ny] != 0) continue;
 
                 visited[nx][ny] = true;
-                queue.add(new Virus(nx, ny));
-                virusCount++;
+                queue.add(new Point(nx, ny));
+                infectedCount++;
             }
         }
 
-        return N * M - virusCount - wallCount;
+        return emptyList.size() - 3 - infectedCount;
     }
 }
