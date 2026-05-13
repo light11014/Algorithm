457f5ab55bd82df7ae1c4f507bc94aef6719b6f8
light11014 <83333513+light11014@users.noreply.github.com>
2026-05-13 21:38:12 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,34 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {};
-
-        Set<String> set = new HashSet<>();
+        Set<String> used = new HashSet<>();
         
-        int size = 0;
-        char last = 0;
-        for(int i = 0; i < words.length; i++) {
-            String word = words[i];
+        used.add(words[0]);
+        
+        for(int i = 1; i < words.length; i++) {
+            String pre = words[i - 1];
+            String cur = words[i];
             
-            if(i == 0) {
-                set.add(word);
-                last = word.charAt(word.length() - 1);
-                size++;
-                continue;
-            } 
+            boolean wrongStart = pre.charAt(pre.length() - 1) != cur.charAt(0);
+            boolean duplicated = !used.add(cur);
             
-            if(word.charAt(0) != last) {
-                return new int[] {i%n+1, i/n+1};
+            if(wrongStart || duplicated) {
+                return new int[] {i % n + 1, i / n + 1};
             }
-                
-            set.add(word);
-            last = word.charAt(word.length() - 1);
-            
-            if(size == set.size()) {
-                return new int[] {i%n+1, i/n+1};
-            }
-                
-            size++;
         }
         
         return new int[] {0, 0};
