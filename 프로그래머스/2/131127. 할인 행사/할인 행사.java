2f9c5433a4b9fa064cfc999e319892ac9c782d65
light11014 <83333513+light11014@users.noreply.github.com>
2026-05-18 22:03:02 +0900

    [level 2] Title: 할인 행사, Time: 16.54 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,31 +1,45 @@
 import java.util.*;
 
 class Solution {
+    Map<String, Integer> wantMap = new HashMap<>();
+    Map<String, Integer> countMap = new HashMap<>();
+    
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        for(int i = 0; i <= discount.length-10; i++) {
-            HashMap<String, Integer> map = refreshMap(want, number);
-            for(int j = 0; j < 10; j++) {
-                if(map.getOrDefault(discount[i+j], 0) == 0) {
-                    break;
-                } else {
-                    if(j == 9) answer++;
-                    map.put(discount[i+j], map.get(discount[i+j])-1);
-                }
+        for(int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+        
+        for(int i = 0; i < 10; i++) {
+            countMap.put(discount[i], countMap.getOrDefault(discount[i], 0) + 1);
+        }
+        
+        if(equalsMaps(want)) {
+            answer++;
+        }
+        
+        for(int s = 0; s < discount.length - 10; s++) {
+            countMap.put(discount[s], countMap.get(discount[s]) - 1);
+            countMap.put(discount[s + 10], countMap.getOrDefault(discount[s + 10], 0) + 1);
+            
+            if(equalsMaps(want)) {
+                answer++;
             }
         }
         
         return answer;
     }
     
-    private HashMap<String, Integer> refreshMap(String[] want, int[] number) {
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for(int i=0; i< want.length; i++) {
-            map.put(want[i], number[i]);
+    public boolean equalsMaps(String[] want) {
+        for(int i = 0; i < want.length; i++) {
+            int w = wantMap.getOrDefault(want[i], 0);
+            int c = countMap.getOrDefault(want[i], 0);
+            
+            if(w != c) {
+                return false;
+            }
         }
-        
-        return map;
+        return true;
     }
 }
