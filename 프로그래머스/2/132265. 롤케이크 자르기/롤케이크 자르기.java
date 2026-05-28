4052d2e6df0cb873eae55bee4893cefd8b38219f
light11014 <83333513+light11014@users.noreply.github.com>
2026-05-28 21:55:59 +0900

    [level 2] Title: 롤케이크 자르기, Time: 110.24 ms, Memory: 154 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        Map<Integer, Integer> right = new HashMap<>();
+        Set<Integer> left = new HashSet<>();
+        
+        for(int t : topping) {
+            right.put(t, right.getOrDefault(t, 0) + 1);
+        }
+        
+        int answer = 0;
+        
+        for(int i = 0; i < topping.length; i++) {
+            int t = topping[i];
+            
+            left.add(t);
+            
+            right.put(t, right.get(t) - 1);
+            
+            if(right.get(t) == 0) {
+                right.remove(t);
+            }
+            
+            if(left.size() == right.size()) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
