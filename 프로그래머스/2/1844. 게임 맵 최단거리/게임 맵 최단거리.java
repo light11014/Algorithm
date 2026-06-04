06a12083d0ec7d882f12242d15fcb9ec62e4f51c
light11014 <83333513+light11014@users.noreply.github.com>
2026-06-04 22:50:03 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.69 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,18 +3,14 @@ import java.util.*;
 class Solution {
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
-    static int N, M;
-    static int[][] dist;
     
     public int solution(int[][] maps) {
-        N = maps.length;
-        M = maps[0].length;
-        
-        dist = new int[N][M];
+        int N = maps.length;
+        int M = maps[0].length;
         
         ArrayDeque<int[]> queue = new ArrayDeque<>();
         queue.add(new int[]{0, 0});
-        dist[0][0] = 1;
+        maps[0][0] = 1;
         
         while(!queue.isEmpty()) {
             int[] cur = queue.poll();
@@ -23,23 +19,17 @@ class Solution {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
                 
-                if(!isRange(nx, ny) || maps[nx][ny] == 0 
-                        || dist[nx][ny] != 0) continue;
+                if(0 > nx || nx >= N || 0 > ny || ny >= M || maps[nx][ny] != 1) continue;
                 
-                dist[nx][ny] = dist[cur[0]][cur[1]] + 1;
+                maps[nx][ny] = maps[cur[0]][cur[1]] + 1;
                 
                 if(nx == N - 1 && ny == M - 1)
-                    return dist[nx][ny];
-                
+                    return maps[nx][ny];
                 
                 queue.add(new int[]{nx, ny});
             }
         }
 
         return -1;
-    }
-    
-    static private boolean isRange(int x, int y) {
-        return 0 <= x && x < N && 0 <= y && y < M;
     }
 }
