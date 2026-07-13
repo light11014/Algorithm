2d5c3b0b9377885ae21a43ac91dde6fac3a0ef19
light11014 <83333513+light11014@users.noreply.github.com>
2026-07-13 09:34:39 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean solution(String s) {
-        return s.matches("\\d{4}|\\d{6}");
+        if(s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        
+        for(char c : s.toCharArray()) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
