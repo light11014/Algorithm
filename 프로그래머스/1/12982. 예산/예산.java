b55105c772e36ef88cb07b2d66563425520cb6e6
light11014 <83333513+light11014@users.noreply.github.com>
2026-07-14 10:01:58 +0900

    [level 1] Title: 예산, Time: 6.80 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        for(int num : d) {
+            if(budget < num) {
+                break;
+            }
+            budget -= num;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
