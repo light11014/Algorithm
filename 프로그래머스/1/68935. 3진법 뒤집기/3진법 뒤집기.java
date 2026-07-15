68270b7887b466a6d04000cc422163c966154244
light11014 <83333513+light11014@users.noreply.github.com>
2026-07-15 09:42:34 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,19 +1,7 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
-        StringBuilder sb = new StringBuilder(convert(n)); 
-        return Integer.parseInt(sb.reverse().toString(), 3);
-    }
-    
-    private String convert(int n) {
-        StringBuilder sb = new StringBuilder();
-        
-        while(n > 0) {
-            sb.insert(0, n % 3);
-            n /= 3;
-        }
-        
-        return sb.toString();
+        String n3 = Integer.toString(n, 3);
+        String reverseN3 = new StringBuilder(n3).reverse().toString();
+        return Integer.parseInt(reverseN3, 3);
     }
 }
