1a643ee1c0d5aec9d461712686e61fa29c5ccdc5
light11014 <83333513+light11014@users.noreply.github.com>
2026-07-20 10:55:59 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < answer.length; i++) {
+            pq.offer(score[i]);
+            
+            while(pq.size() > k) {
+                pq.poll();
+            }
+            
+            answer[i] = pq.peek();
+        }
+        
+        return answer;
+    }
+}
