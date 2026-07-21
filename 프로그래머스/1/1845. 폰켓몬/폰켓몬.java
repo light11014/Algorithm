be95e7fd9dda0b435fbb94b835f231bec1821233
light11014 <83333513+light11014@users.noreply.github.com>
2026-07-21 09:20:15 +0900

    [level 1] Title: 폰켓몬, Time: 11.28 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,7 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        // Arrays.stream(nums).boxed().collect(Collectors.toCollection(HashSet::new));
-        return Math.min((int)Arrays.stream(nums).boxed().distinct().count(), nums.length/2);
+        // 1. nums 배열로 종류 -> 개수 Map 생성
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        // 2. N/2(최대 뽑을 수 있는 횟수)와 key 개수(종류 수) 중 작은 값 반환
+        return Math.min(nums.length / 2, map.size());
     }
 }
