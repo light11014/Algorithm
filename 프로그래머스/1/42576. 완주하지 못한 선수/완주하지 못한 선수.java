7527820dbf53c61e2a63f0ef98d2b009d15ee327
light11014 <83333513+light11014@users.noreply.github.com>
2026-07-29 09:19:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.08 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,19 +4,18 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
         
-        for(String name : completion) {
+        for(String name : participant) {
             map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
+        for(String name : completion) {
+            map.put(name, map.get(name) - 1);
+        }
+        
         for(String name : participant) {
-            if(!map.containsKey(name))
+            if(map.get(name) > 0) {
                 return name;
-            
-            if(map.get(name) == 1) {
-                map.remove(name);
-            } else {
-                map.put(name, map.get(name) - 1);
-            }
+            } 
         }
         
         return "";
