fc1809773bff52ef20449ed7cf5e0c1e2e6a5fc6
light11014 <83333513+light11014@users.noreply.github.com>
2026-07-30 10:07:40 +0900

    [level 1] Title: 햄버거 만들기, Time: 9.07 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,33 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] ingredient) {
-        int[] burger = {1, 3, 2, 1};
+        int[] stack = new int[ingredient.length];
+        int size = 0;
         int answer = 0;
         
-        Deque<Integer> stack = new ArrayDeque<>();
-        for(int i : ingredient) {
-            stack.push(i);
+        for(int value : ingredient) {
+            stack[size++] = value;
             
-            while(stack.size() >= 4) {
-                boolean match = true;
-                
-                for(int j = 0; j < 4; j++) {
-                    if(stack.peek() == burger[j]) {
-                        stack.pop();
-                    } else {
-                        for(int k = j - 1; k >= 0; k--) {
-                            stack.push(burger[k]);
-                        }
-                        match = false;
-                        break;
-                    }
-                }
-                
-                if(match) {
-                    answer++;
-                } else {
-                    break;
-                }
+            if(size >= 4 
+               && stack[size - 4] == 1 
+               && stack[size - 3] == 2
+               && stack[size - 2] == 3
+               && stack[size - 1] == 1) {
+                size -= 4;
+                answer++;
             }
         }
         
