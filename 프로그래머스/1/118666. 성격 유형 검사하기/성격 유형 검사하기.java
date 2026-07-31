8708c20b9086a4bb64bd610c54b541895d55e146
light11014 <83333513+light11014@users.noreply.github.com>
2026-07-31 10:56:39 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 5.38 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -2,31 +2,34 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        
-        // Map을 쓰면 좋겠다
         Map<Character, Integer> scores = new HashMap<>();
         
-        // survey와 choices를 순회하면서 점수 갱신
         for(int i = 0; i < survey.length; i++) {
-            int num = 4 - choices[i];
-            
-            if(num == 0) continue;
-            else if(num > 0) {
-                char c = survey[i].charAt(0);
-                scores.put(c, scores.getOrDefault(c, 0) + num);
-            } else {
-                char c = survey[i].charAt(1);
-                scores.put(c, scores.getOrDefault(c, 0) - num);
+            int score = Math.abs(choices[i] - 4);
+
+            if (choices[i] < 4) {
+                char type = survey[i].charAt(0);
+                scores.put(type, scores.getOrDefault(type, 0) + score);
+            } else if (choices[i] > 4) {
+                char type = survey[i].charAt(1);
+                scores.put(type, scores.getOrDefault(type, 0) + score);
             }
         }
         
-        // 마지막에 1~4번 지표까지 확인하고 성격유형 반환
-        StringBuilder sb = new StringBuilder();
-        sb.append(scores.getOrDefault('R', 0) >= scores.getOrDefault('T', 0)? "R" : "T");
-        sb.append(scores.getOrDefault('C', 0) >= scores.getOrDefault('F', 0)? "C" : "F");
-        sb.append(scores.getOrDefault('J', 0) >= scores.getOrDefault('M', 0)? "J" : "M");
-        sb.append(scores.getOrDefault('A', 0) >= scores.getOrDefault('N', 0)? "A" : "N");
-        
-        return sb.toString();
+        StringBuilder answer = new StringBuilder();
+
+        answer.append(getType(scores, 'R', 'T'));
+        answer.append(getType(scores, 'C', 'F'));
+        answer.append(getType(scores, 'J', 'M'));
+        answer.append(getType(scores, 'A', 'N'));
+
+        return answer.toString();
+    }
+    
+    private char getType(Map<Character, Integer> scores, char first, char second) {
+        int firstScore = scores.getOrDefault(first, 0);
+        int secondScore = scores.getOrDefault(second, 0);
+
+        return firstScore >= secondScore? first : second;
     }
 }
