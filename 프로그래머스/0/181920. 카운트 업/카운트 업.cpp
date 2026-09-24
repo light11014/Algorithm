acad75c993901538a2dcfe1eecbbcd30871bda4b
light11014 <83333513+light11014@users.noreply.github.com>
2026-09-24 15:32:45 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int start_num, int end_num) {
+    vector<int> answer;
+    
+    for(int i = 0; i <= end_num - start_num; i++) {
+        answer.push_back(i + start_num);
+    }
+    
+    return answer;
+}
