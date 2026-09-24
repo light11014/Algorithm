88140bd88a9349b9d340d18a2fa182bae1c7ae58
light11014 <83333513+light11014@users.noreply.github.com>
2026-09-24 15:48:06 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.85 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int solution(vector<int> num_list) {
-    int a = accumulate(num_list.begin(), num_list.end(), 0L);
+    int a = accumulate(num_list.begin(), num_list.end(), 0);
     int b = accumulate(num_list.begin(), num_list.end(), 1, multiplies<int>());
     
     return a * a > b;
