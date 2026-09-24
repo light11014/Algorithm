4d54344a3bed04abbc053a4eb8312242275f17d7
light11014 <83333513+light11014@users.noreply.github.com>
2026-09-24 15:41:34 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 4.88 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -5,11 +5,5 @@ using namespace std;
 vector<int> solution(vector<int> num_list) {
     sort(num_list.begin(), num_list.end());
     
-    vector<int> answer;
-    
-    for(int i = 0; i < 5; i++) {
-        answer.push_back(num_list[i]);
-    }
-    
-    return answer;
+    return vector<int>(num_list.begin(), num_list.begin() + 5);
 }
