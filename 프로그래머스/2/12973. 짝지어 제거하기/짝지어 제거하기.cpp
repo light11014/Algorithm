3ad874c31416e27cac11f3192327d892cce4ecbb
light11014 <83333513+light11014@users.noreply.github.com>
2026-09-26 17:24:54 +0900

    [level 2] Title: 짝지어 제거하기, Time: 3.69 ms, Memory: 6.73 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -6,9 +6,7 @@ int solution(string s)
     stack<char> st;
     
     for(char c : s) {
-        if(st.empty()) {
-            st.push(c);
-        } else if (c == st.top()) {
+        if(!st.empty() && c == st.top()) {
             st.pop();
         } else{
             st.push(c);
