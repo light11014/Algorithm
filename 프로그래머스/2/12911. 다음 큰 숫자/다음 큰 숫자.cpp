a50271dc780c43c3001bb5de6b0766794e6085f7
light11014 <83333513+light11014@users.noreply.github.com>
2026-09-26 17:47:03 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 4.67 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -2,37 +2,14 @@
 
 using namespace std;
 
-string tobinary(int n);
-
 int solution(int n) {
-    int answer = 0;
+    int one_count = __builtin_popcount(n);
     
-    string binary_n = tobinary(n);
-    int one_count = count(binary_n.begin(), binary_n.end(), '1');
+    int next_num = n + 1;
     
-    int next_num = n;
-    
-    int next_count;
-    do {
+    while(one_count != __builtin_popcount(next_num)) {
         next_num++;
-        
-        string binary_next = tobinary(next_num);
-        next_count = count(binary_next.begin(), binary_next.end(), '1');
-        
-    } while(next_count != one_count);
-    
-    return next_num;
-}
-
-string tobinary(int n) {
-    string result;
-    
-    while(n > 0) {
-        result += (n % 2) + '0';
-        n /= 2;
     }
-    
-    reverse(result.begin(), result.end());
-    
-    return result;
+
+    return next_num;
 }
